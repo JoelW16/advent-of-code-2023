7c02fb68ee8bda608bbfe2d6724c70842a5d10ae
Joel Walker <hello@joelwalker.co.uk>
2023-12-03 22:12:04 +0000

    day:3 Calculate part number sum

--- day3/partNumbers.go
+++ day3/partNumbers.go
@@ -3,48 +3,97 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	_, err := os.ReadFile("input.txt")
+	content, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
 	}
+	part1 := FindPartNumbers(string(content))
+	fmt.Println(part1)
 }
 
 func CreatePartMap(input string) []string {
 	return strings.Split(input, "\n")
 }
 
-func CheckAdjacentParts(partMap []string, x int, y int) bool {
-	isTop := partMap[y][x-1] == '*'
-	isBottom := partMap[y][x+1] == '*'
-	isRight := partMap[y-1][x] == '*'
-	isLeft := partMap[y+1][x] == '*'
-
-	isTopRight := partMap[y-1][x-1] == '*'
-	isBottomRight := partMap[y+1][x-1] == '*'
-	isTopLeft := partMap[y-1][x+1] == '*'
-	isBottomLeft := partMap[y+1][x+1] == '*'
-
-	return isTop || isBottom || isRight || isLeft || isTopRight || isBottomRight || isTopLeft || isBottomLeft
+func SearchIndexes(partMap []string, startIndex int, endIndex int, row int) bool {
+	symbolRegex := regexp.MustCompile(`[^0-9.]`)
+	search := partMap[row][startIndex : endIndex+1]
+	return symbolRegex.MatchString(search)
 }
 
-func FindPartNumbers(input string) []int {
-	partMap := CreatePartMap(input)
-	fmt.Println(partMap)
+func GetStartIndex(numberXIndexes []int) int {
+	startIndex := numberXIndexes[0]
+	if startIndex > 0 {
+		startIndex--
+	}
+	return startIndex
+}
 
+func GetEndIndex(numberXIndexes []int, partMap []string, row int) int {
+	endIndex := numberXIndexes[len(numberXIndexes)-1] - 1
+	if endIndex < len(partMap[row])-1 {
+		endIndex++
+	}
+	return endIndex
+}
+
+func isSymbolAbove(partMap []string, numberXIndexes []int, row int) bool {
+	if row == 0 {
+		return false
+	}
+	startIndex := GetStartIndex(numberXIndexes)
+	endIndex := GetEndIndex(numberXIndexes, partMap, row)
+	return SearchIndexes(partMap, startIndex, endIndex, row-1)
+}
+
+func isSymbolBelow(partMap []string, numberXIndexes []int, row int) bool {
+	if row == len(partMap)-1 {
+		return false
+	}
+	startIndex := GetStartIndex(numberXIndexes)
+	endIndex := GetEndIndex(numberXIndexes, partMap, row)
+	return SearchIndexes(partMap, startIndex, endIndex, row+1)
+}
+
+func isSymbolOnLeft(partMap []string, numberXIndexes []int, row int) bool {
+	if numberXIndexes[0] == 0 {
+		return false
+	}
+	startIndex := GetStartIndex(numberXIndexes)
+	return SearchIndexes(partMap, startIndex, startIndex, row)
+}
+
+func isSymbolOnRight(partMap []string, numberXIndexes []int, row int) bool {
+	if numberXIndexes[len(numberXIndexes)-1] == len(partMap[row])-1 {
+		return false
+	}
+	endIndex := GetEndIndex(numberXIndexes, partMap, row)
+	return SearchIndexes(partMap, endIndex, endIndex, row)
+}
+
+func CheckAdjacentSymbols(partMap []string, numberXIndexes []int, row int) bool {
+	return isSymbolAbove(partMap, numberXIndexes, row) || isSymbolBelow(partMap, numberXIndexes, row) || isSymbolOnLeft(partMap, numberXIndexes, row) || isSymbolOnRight(partMap, numberXIndexes, row)
+}
+
+func FindPartNumbers(input string) int {
+	partMap := CreatePartMap(input)
+	numberRegex := regexp.MustCompile(`\d+`)
+	sum := 0
 	for y, row := range partMap {
-		fmt.Println(y, row)
-		for x, part := range row {
-			if _, err := strconv.Atoi(string(part)); err == nil {
-				fmt.Printf("%q looks like a number.\n", string(part))
-				check := CheckAdjacentParts(partMap, x, y)
-				fmt.Println(check)
+		numberIndexes := numberRegex.FindAllStringIndex(row, -1)
+		numbers := numberRegex.FindAllString(row, -1)
+		for i, numberIndex := range numberIndexes {
+			if CheckAdjacentSymbols(partMap, numberIndex, y) {
+				num, _ := strconv.Atoi(numbers[i])
+				sum += num
 			}
 		}
 	}
-	return []int{1, 12}
+	return sum
 }
--- day3/partNumbers_test.go
+++ day3/partNumbers_test.go
@@ -7,11 +7,30 @@ import (
 func TestFindPartNumbersSmall(t *testing.T) {
 	input :=
 		`467..114..
-	...*......
-	..35..633.`
+...*......
+..35..633.`
 	result := FindPartNumbers(input)
 
-	if len(result) != 2 || result[0] != 467 || result[1] != 35 {
-		t.Errorf("Expected [467 35], got %d", result)
+	if result != 502 {
+		t.Errorf("Expected 502, got %d", result)
+	}
+}
+
+func TestFindPartNumbers(t *testing.T) {
+	input :=
+		`467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+	result := FindPartNumbers(input)
+
+	if result != 4361 {
+		t.Errorf("Expected 4361, got %d", result)
 	}
 }
