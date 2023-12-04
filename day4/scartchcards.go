485cbb9d81f4116027d0077c70241567ff7d2ddb
Joel Walker <hello@joelwalker.co.uk>
2023-12-04 14:50:43 +0000

    day:4 Find number of scratchcards played

--- day4/scartchcards.go
+++ day4/scartchcards.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -15,6 +16,9 @@ func main() {
 
 	part1 := FindSumOfWinningValues(string(content))
 	fmt.Println(part1)
+
+	part2 := FindSumOfPlayableCards(string(content))
+	fmt.Println(part2)
 }
 
 func IsWinningNumber(number string, winningNumbers []string) bool {
@@ -55,4 +59,40 @@ func FindSumOfWinningValues(input string) int {
 		}
 	}
 	return totalScore
+}
+
+func UpdatePlayableCardCount(input string, cardCountMap map[int]int) map[int]int {
+	card := strings.Split(input, ":")
+	numbers := strings.Split(card[1], "|")
+	numberRegex := regexp.MustCompile(`[0-9]+`)
+	playerNumbers := numberRegex.FindAllString(numbers[0], -1)
+	winningNumbers := numberRegex.FindAllString(numbers[1], -1)
+	cardNumber, _ := strconv.Atoi(numberRegex.FindString(card[0]))
+	score := 0
+
+	for _, playerNumber := range playerNumbers {
+		if IsWinningNumber(playerNumber, winningNumbers) {
+			score++
+		}
+	}
+	cardCountMap[cardNumber] += 1
+	for i := 1; i <= score; i++ {
+		cardCountMap[cardNumber+i] += cardCountMap[cardNumber]
+	}
+
+	return cardCountMap
+}
+
+func FindSumOfPlayableCards(input string) int {
+	cards := strings.Split(input, "\n")
+	cardCountMap := make(map[int]int)
+	for _, card := range cards {
+		cardCountMap = UpdatePlayableCardCount(card, cardCountMap)
+	}
+
+	sum := 0
+	for _, count := range cardCountMap {
+		sum += count
+	}
+	return sum
 }
